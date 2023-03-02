d3a27cf841d77f12f8dfc908221e60fc1b9aa478
Stewart <44583600+stewheart123@users.noreply.github.com>
2023-03-02 00:34:54 +0000

    slider improvements

--- src/components/QuestionBlock.js
+++ src/components/QuestionBlock.js
@@ -1,32 +1,111 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Navigation, Pagination } from "swiper";
 
-//perhaps split each panel into a separate component
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+//perhaps split each panel into a separate component?
+
+//add onclick event that changes the state plus updates an object / array with answers
+
+//once all are complete, slider is replaced with a modal - 'ready for results?' / cancel - undos last array entery or shows modal 
+
+//modal also has a reset button
 class QuestionBlock extends React.Component {
   render() {
     return (
       <div className="question-block">
-        <div className="slide-wrapper">
-          <div className="question-block__question-container">
-            <h1>Which sounds most like you?</h1>
-          </div>
-          <div className="question-block__rectangle-button-container">
-            <div className="question-block__rectangle-button">
-              <p>Resilient</p>
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          speed={1000}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="slide-wrapper">
+              <div className="question-block__question-container">
+                <h1>Which sounds most like you?</h1>
+              </div>
+              <div className="question-block__rectangle-button-container">
+                <div className="question-block__rectangle-button">
+                  <p>Resilient</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Great with people</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Friendly</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Logical thinker</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Eye for numbers</p>
+                </div>
+              </div>
             </div>
-            <div className="question-block__rectangle-button">
-              <p>Great with people</p>
+          </SwiperSlide>
+          <SwiperSlide>
+          
+            <div className="slide-wrapper">
+              <div className="question-block__question-container">
+                <h1>Another Question?</h1>
+              </div>
+              <div className="question-block__rectangle-button-container">
+                <div className="question-block__rectangle-button">
+                  <p>Resilient</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Great with people</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Friendly</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Logical thinker</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Eye for numbers</p>
+                </div>
+              </div>
             </div>
-            <div className="question-block__rectangle-button">
-              <p>Friendly</p>
+          </SwiperSlide>
+          <SwiperSlide>
+         
+            <div className="slide-wrapper">
+              <div className="question-block__question-container">
+                <h1>Question 2</h1>
+              </div>
+              <div className="question-block__rectangle-button-container">
+                <div className="question-block__rectangle-button">
+                  <p>Resilient</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Great with people</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Friendly</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Logical thinker</p>
+                </div>
+                <div className="question-block__rectangle-button">
+                  <p>Eye for numbers</p>
+                </div>
+              </div>
             </div>
-            <div className="question-block__rectangle-button">
-              <p>Logical thinker</p>
-            </div>
-            <div className="question-block__rectangle-button">
-              <p>Eye for numbers</p>
-            </div>
-          </div>
-        </div>
+          </SwiperSlide>
+        </Swiper>
       </div>
     );
   }
